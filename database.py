d307a30212430b10a259db3ea442258911dfa39e
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2023-10-12 17:36:50 +0300

    Values updated

--- database.py
+++ database.py
@@ -1,14 +1,23 @@
 import sqlite3
 import hashlib
 
-conn = sqlite3.connect("userdata.db")
-cur = conn.cursor()
+class DatabaseHandler:
+    def __init__(self, db_file):
+        self.db_file = db_file
 
-cur.execute("""
-CREATE TABLE IF NOT EXISTS userdata(
-    id INTEGER PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-    )
-""")
+    def connect(self):
+        self.conn = sqlite3.connect(self.db_file)
+        self.cur = self.conn.cursor()
 
+    def disconnect(self):
+        self.conn.commit()
+        self.conn.close()
+
+    def create_user(self, username, password):
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        self.cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username, hashed_password))
+
+    # Add more database operations as needed
+
+# Instantiate the database handler
+db_handler = DatabaseHandler("userdata.db")
